8e8b081677246f3c14d70dc40072ec3d41816ac7
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-04-08 02:30:04 -0600

    detail bachhoa

--- js/main.js
+++ js/main.js
@@ -148,4 +148,16 @@ $(document).ready(function() {
 						}
 				}
 	});
+	$('.gallery-bachhoa').on('initialized.owl.carousel changed.owl.carousel', function(e) {
+    if (!e.namespace)  {
+      return;
+    }
+    var carousel = e.relatedTarget;
+    $('.slider-counter').text(carousel.relative(carousel.current()) + 1 + '/' + carousel.items().length);
+  }).owlCarousel({
+    items: 1,
+    loop:true,
+		margin:0,
+		dots: true,
+	});
 });
